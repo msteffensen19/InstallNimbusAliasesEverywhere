c060886325bfe37ecf64b1e5860b516c68651618
Mark Steffensen <mark.steffensen@microfocus.com>
2022-11-30 15:54:37 -0700

    Update InstallNimbusAliases.bat

--- Windows/InstallNimbusAliases.bat
+++ Windows/InstallNimbusAliases.bat
@@ -3,7 +3,7 @@
 :: 2. Copy the Microsoft profile into the PowerShell Directory
 :: 3. Create a registry key for the doskey.bat file
 ::
-:: The result is to add the common Nimbus aliases (dps, dpsa, di, de) to both cmd and PowerShell windows
+:: The result is to add the common Nimbus docker aliases (dps, dpsa, di, de) to both cmd and PowerShell windows
 
 copy doskey.txt "C:\Program Files\Docker\doskey.bat"
 copy Microsoft.PowerShell_profile.ps1 %HOMEDRIVE%%HOMEPATH%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
