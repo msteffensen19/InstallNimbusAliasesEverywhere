062db77f21c457a3c5b6c2869b560ec48e38fd0e
Mark Steffensen <mark.steffensen@microfocus.com>
2023-03-15 09:54:32 -0600

    Added container ID

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,8 @@
 
 # Powershell profile (.bashrc equiv) is located C:\Users\demo\Documents\WindowsPowerShell
 # as file Microsoft.PowerShell_profile.ps1
-function dps {docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Image}}"}
-function dpsa {docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Image}}"}
+function dps {docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Image}}\t{{.ID}}"}
+function dpsa {docker ps -a --format "table {{.Names}}\t{{.Status}}\t{{.Image}}\t{{.ID}}"}
 function di {docker images | sort }
 function de {docker exec -it $args[0] powershell}
 function myip {ipconfig | findstr /i "ipv4" | sed -n 1p | sed s/^.......................................//}
